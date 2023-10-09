bc1f7699584509e34e971912c4ff0938ca0c9aee
unknown <ulanovnikolay10p@gmail.com>
2023-10-09 19:30:19 +0300

    initialization

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify';
+
+const server = Fastify();
+
+async function main() {
+    try {
+        await server.listen({
+            port: 3000,
+            host: '0.0.0.0',
+        });
+
+        console.log(`Server ready at http://localhost:3000`);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+main();
